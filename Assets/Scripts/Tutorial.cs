c8e0e94333d0ed42024f0bbdfeb36e888486ab06
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2022-10-20 13:04:11 +0200

    tuto

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
+    private bool isTutorialFinish = false;
+    void Start()
+    {
+        foreach(GameObject go in gameObjects)
+        {
+            go.GetComponent<Outline>().OutlineWidth = 10;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TutorialFinished()
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            go.GetComponent<Outline>().OutlineWidth = 0;
+        }
+    }
+}
